320fdecac98db7b385b9e7538d1f569c1470e3de
MikeTX56 <tmikiyas22@gmail.com>
2026-02-04 01:37:19 +0300

    correct the path for model

--- projects/classification/iris_classification/iris_app/app.py
+++ projects/classification/iris_classification/iris_app/app.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import joblib
 import numpy as np
+import os
 
+# Get the directory that this script (app.py) is in
+base_path = os.path.dirname(__file__)
+model_path = os.path.join(base_path, 'iris_random_forest_model.joblib')
 @st.cache_resource
 def load_model():
-    return joblib.load("iris_random_forest_model.joblib")
+    return joblib.load(model_path)
 model = load_model()
 st.title("Iris Species Predictor")
 # 1. Manually set ranges based on our original data we used to train
@@ -17,7 +21,7 @@ features = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
 
 if st.button('Predict Species'):
     prediction = model.predict(features)
-    
+
     species = ['Setosa', 'Versicolor', 'Virginica']
     result = species[prediction[0]]
     
