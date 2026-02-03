2ef0ee46abb7ffa8bd5427a0563515fbebf8d95d
MikeTX56 <tmikiyas22@gmail.com>
2026-02-04 01:23:55 +0300

    deploying first web app

---
+++ projects/classification/iris_classification/iris_app/app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+@st.cache_resource
+def load_model():
+    return joblib.load("iris_random_forest_model.joblib")
+model = load_model()
+st.title("Iris Species Predictor")
+# 1. Manually set ranges based on our original data we used to train
+sepal_length = st.slider("sepal length (cm)", 4.3, 7.9, 5.8)
+sepal_width = st.slider("sepal width (cm)", 2.0, 4.4, 3.0)
+petal_length = st.slider("petallength  (cm)",1.0, 6.9, 3.7)
+petal_width = st.slider("petal width (cm)", 0.1, 2.5, 1.2)
+
+features = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+
+if st.button('Predict Species'):
+    prediction = model.predict(features)
+    
+    species = ['Setosa', 'Versicolor', 'Virginica']
+    result = species[prediction[0]]
+    
+    st.success(f"The predicted species is: **{result}**")
